759c2fb2b6d96079f1135d504da66e606abd148b
Yassine BOUKHLA <yassine.boukhla@teal.ma>
2024-01-18 16:22:54 +0000

    InputBox UI Done

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -5,20 +5,29 @@ import {
   Text,
   ImageBackground,
   FlatList,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import Message from "../components/Message";
 import messages from "../../assets/data/messages.json";
 import bg from "../../assets/images/BG.png";
+import InputBox from "../components/InputBox";
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList
-        data={messages}
-        renderItem={({ item }) => <Message message={item} />}
-        style={styles.list}
-        inverted
-      />
-    </ImageBackground>
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={styles.bg}
+    >
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
