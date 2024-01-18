386a6f35cdd27f2533e4e82d6b40175672b5f490
Yassine BOUKHLA <yassine.boukhla@teal.ma>
2024-01-18 14:19:52 +0000

    ChatsScreen name change

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import ChatScreen from "./src/screens/ChatScreen";
+import ChatsScreen from "./src/screens/ChatsScreen";
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatScreen />
+      <ChatsScreen />
     </View>
   );
 }
